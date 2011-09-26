a795aa415db81bddfc1de2883ef27aea87154894
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2011-09-26 12:18:02 +0200

    First "working" version.
    Python 3.3.0a0 (default:016543a9333a, Sep 22 2011, 19:14:22)
    [GCC 4.5.2] on linux
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import termsize
    >>> termsize.termsize()
    (92, 33)
    >>> termsize.termsize(2)
    (92, 33)
    >>> termsize.termsize(3)
    (0, 0)

---
+++ termsize.c
@@ -0,0 +1,34 @@
+#include <Python.h>
+
+#include <sys/ioctl.h>
+
+static PyObject* _termsize(PyObject *self, PyObject *args)
+{
+	int fd = 1; /* stdout */
+
+	if(!PyArg_ParseTuple(args, "|i", &fd))
+		return NULL;
+	
+	struct winsize w;
+	ioctl(fd, TIOCGWINSZ, &w);
+
+	return Py_BuildValue("(ii)", w.ws_col, w.ws_row);
+}
+
+static PyMethodDef methods[] = {
+	{"termsize", _termsize, METH_VARARGS,
+	 "Return the size of the terminal window as (columns, rows)."},
+	{NULL, NULL, 0, NULL} /* sentinel */
+};
+
+static struct PyModuleDef termsize = {
+	PyModuleDef_HEAD_INIT,
+	.m_name = "termsize",
+	.m_size = 0,
+	.m_methods = methods,
+};
+
+PyMODINIT_FUNC PyInit_termsize(void)
+{
+	return PyModule_Create(&termsize);
+}
